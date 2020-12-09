c5081b61187c1f23bd42fcc72033667871185318
pragati1610 <pragatibhattad1610@gmail.com>
2020-12-09 10:16:29 +0530

    oAuth

--- Login/routes/login.js
+++ Login/routes/login.js
@@ -7,123 +7,120 @@ const mongoose = require("mongoose");
 const { User } = require("../models/model.js");
 
 router.post("/signup", (req, res) => {
-  let mail = req.body.email;
-  let pass = req.body.password;
-  console.log(mail);
-  User.find({
-    email: req.body.email,
-  })
-    .exec()
-    .then((useri) => {
-      if(pass.length < 7){
-        return res.status(422).json({
-          message: "length of password should be greater than 6 letters"
-        });
-      }
-      if (useri.length >= 1) {
-        return res.status(409).json({
-          message: "mail exists",
-        });
-      } else {
-
-        const secret_key = process.env.SECRET_KEY;
-        const token1 = req.body.token;
-        const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token1}`;
-
-        fetch(url, {
-            method: 'post'
+    let mail = req.body.email;
+    let pass = req.body.password;
+    console.log(mail);
+    User.find({
+            email: req.body.email,
         })
-            .then(response => response.json())
-            .then(google_response => res.json({ google_response }))
-            .catch(error => return res.json({ error }));
+        .exec()
+        .then((useri) => {
+            if (pass.length < 7) {
+                return res.status(422).json({
+                    message: "length of password should be greater than 6 letters"
+                });
+            }
+            if (useri.length >= 1) {
+                return res.status(409).json({
+                    message: "mail exists",
+                });
+            } else {
 
-        bcrypt.genSalt(parseInt(process.env.NUM_HASH), function(err, salt) {
-          bcrypt.hash(req.body.password, salt, function(err, hash) {
-              if (err) {
-                    return res.status(500).json({
-                      error: err,
+                const secret_key = process.env.SECRET_KEY;
+                const token1 = req.body.token;
+                const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token1}`;
+
+                fetch(url, {
+                        method: 'post'
+                    })
+                    .then(response => response.json())
+                    .then(google_response => res.json({ google_response }))
+                    .catch(error => { return res.json({ error }) });
+
+                bcrypt.genSalt(parseInt(process.env.NUM_HASH), function(err, salt) {
+                    bcrypt.hash(req.body.password, salt, function(err, hash) {
+                        if (err) {
+                            return res.status(500).json({
+                                error: err,
+                            });
+                        } else {
+                            const user = new User({
+                                _id: new mongoose.Types.ObjectId(),
+                                email: req.body.email,
+                                password: hash,
+                            });
+                            user.save().then((result) => {
+                                const token = jwt.sign({
+                                        email: user.email,
+                                        userId: user._id,
+                                    },
+                                    process.env.JWT_KEY, {
+                                        expiresIn: "2d",
+                                    }
+                                );
+                                res.status(201).json({
+                                    message: "User created",
+                                    userid: user._id,
+                                    token: token,
+                                });
+                            });
+                        }
                     });
-                  } else {
-                    const user = new User({
-                      _id: new mongoose.Types.ObjectId(),
-                      email: req.body.email,
-                      password: hash,
-                    });
-                    user.save().then((result) => {
-                      const token = jwt.sign({
-                            email: user.email,
-                            userId: user._id,
-                          },
-                          process.env.JWT_KEY,
-                          {
-                            expiresIn: "2d",
-                          }
-                        );
-                      res.status(201).json({
-                        message: "User created",
-                        userid: user._id,
-                        token: token,
-                      });
-                    });
-                 }
-          });
-      });
-    }
-  })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err,
+            });
+        });
 });
 
 
 router.post("/login", (req, res) => {
-  User.find({
-    email: req.body.email,
-  })
-    .exec()
-    .then((user) => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed",
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth failed",
-          });
-        }
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h",
+    User.find({
+            email: req.body.email,
+        })
+        .exec()
+        .then((user) => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed",
+                });
             }
-          );
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token,
-            userid: user[0]._id,
-          });
-        }
-        return res.status(401).json({
-          message: "Auth failed",
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Auth failed",
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                            email: user[0].email,
+                            userId: user[0]._id,
+                        },
+                        process.env.JWT_KEY, {
+                            expiresIn: "1h",
+                        }
+                    );
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token,
+                        userid: user[0]._id,
+                    });
+                }
+                return res.status(401).json({
+                    message: "Auth failed",
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json({
+                error: err,
+            });
         });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).json({
-        error: err,
-      });
-    });
 });
 
 module.exports = router;
--- Login/routes/oAuth.js
+++ Login/routes/oAuth.js
@@ -3,94 +3,73 @@ const router = express.Router();
 const axios = require("axios");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const {User} = require("../models/model.js");
+const { User } = require("../models/model.js");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 
-async function getAccessTokenFromCode(code) {
-  const { data } = await axios({
-    url: `https://oauth2.googleapis.com/token`,
-    method: "post",
-    data: {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: process.env.REDIRECT_URI,
-      grant_type: "authorization_code",
-      code,
-    },
-  });
-  console.log(data); // { access_token, expires_in, token_type, refresh_token }
-  return data.access_token;
-}
 
-async function getGoogleDriveFiles(access_token) {
-  const { data } = await axios({
-    url: "https://www.googleapis.com/oauth2/v2/userinfo",
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-  console.log(data); // { id, email, given_name, family_name }
-  return data;
-}
 
-router.get("/google", async (req, res) => {
-  try {
-    let token = await getAccessTokenFromCode(req.query.code);
-    console.log(token);
-    let data = await getGoogleDriveFiles(token);
-    let mail = data.email;
-    let token1;
-    let user = await User.find({
-      email: mail,
-    }).exec();
+var admin = require("firebase-admin");
 
-    if (user.length >= 1) {
-      token1 = jwt.sign(
-        {
-          email: user[0].email,
-          userId: user[0].id,
-        },
-        process.env.JWT_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
-      return res.status(200).json({
-        message: "Auth successful",
-        token: token1,
-      });
-    } else {
-      const useri = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: mail,
-      });
-      useri.save().then((result) => {
-        res.status(201).json({
-          message: "User created",
-          id: useri._id,
-        });
-      });
-      token1 = jwt.sign(
-        {
-          email: useri.email,
-          userId: useri._id,
-        },
-        process.env.JWT_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
-      return res.status(200).json({
-        message: "Auth successful",
-        token: token1,
-        id: useri._id,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
+var serviceAccount = require("../../firebase.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://groff-oauth-default-rtdb.firebaseio.com"
 });
+
+router.post("/google", async(req, res) => {
+    try {
+        const adminInstance = await admin.auth()
+        const userInfo = await adminInstance.verifyIdToken(req.body.idToken);
+
+        const mail = userInfo.email;
+
+        let token1;
+        let user = await User.find({
+            email: mail,
+        }).exec();
+
+        if (user.length >= 1) {
+            token1 = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0].id
+                },
+                process.env.JWT_KEY, {
+                    expiresIn: "1h",
+                }
+            );
+            return res.status(200).json({
+                message: "Auth successful",
+                token: token1,
+            });
+        } else {
+            const useri = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: mail,
+            });
+            useri.save().then((result) => {
+                console.log(result);
+            });
+            token1 = jwt.sign({
+                    email: useri.email,
+                    userId: useri._id,
+                },
+                process.env.JWT_KEY, {
+                    expiresIn: "1h",
+                }
+            );
+            return res.status(200).json({
+                message: "Auth successful",
+                token: token1,
+                id: useri.id,
+                email: useri.email
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 
 module.exports = router;
